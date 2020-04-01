5a0a35784317df3ff7abbc39ece892bf4bdfd1cc
Ravi Teja Surla <raviteja1706@gmail.com>
2020-04-01 10:36:09 +0530

    add cheese model tests

---
+++ everycheese/cheeses/tests/test_models.py
@@ -0,0 +1,17 @@
+import pytest
+
+from ..models import Cheese
+
+
+# Connects our tests with our database
+pytestmark = pytest.mark.django_db
+
+def test___str__(): 
+    cheese = Cheese.objects.create(
+        name="Stracchino", 
+        description="Semi-sweet cheese that goes well with starches.", 
+        firmness=Cheese.Firmness.SOFT,
+    )
+
+    assert cheese.__str__() == "Stracchino" 
+    assert str(cheese) == "Stracchino"
