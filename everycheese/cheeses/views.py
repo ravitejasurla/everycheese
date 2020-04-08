eda1c8bbbcbf9fcc521bf8dda7e00e005b83c51c
Ravi Teja Surla <raviteja1706@gmail.com>
2020-04-08 23:11:19 +0530

    Constrain Add Cheese form to logged-in users only

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views.generic import CreateView
 
 from django.views.generic import ListView, DetailView
@@ -7,7 +9,7 @@ from .models import Cheese
 class CheeseListView(ListView): 
     model = Cheese
 
-class CheeseCreateView(CreateView): 
+class CheeseCreateView(LoginRequiredMixin, CreateView): 
     model = Cheese
     fields = ['name', 'description', 'firmness', 'country_of_origin']
 
